0eb3d070855e7c3e09af3d35d197fcc5872ef2ff
Brentuzi <90348225+Brentuzi@users.noreply.github.com>
2022-02-24 20:12:00 +0300

    Add files via upload

---
+++ LAB 8 LINQ 4/LINQsqlM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQsqlM4
+{
+	public partial class LINQsqlM : Form
+	{
+		public LINQsqlM()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonShowTop10_Click(object sender, EventArgs e)
+		{
+			var db = new DataClassesProcDataContext();
+			foreach (var r in db.Ten_Most_Expensive_Products())
+			{
+				ListViewItem item = listView10Most.Items.Add(r.TenMostExpensiveProducts.ToString());
+				item.SubItems.Add(r.UnitPrice.ToString());
+			}
+		}
+
+        private void LINQsqlM_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
